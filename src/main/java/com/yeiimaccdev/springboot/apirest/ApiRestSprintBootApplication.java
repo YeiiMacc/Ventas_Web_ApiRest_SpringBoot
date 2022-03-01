3a091a79ce2526202130854624985efb816cd228
YeiiMacc <yeiimaccdev@gmail.com>
2022-02-28 23:07:48 -0500

    first commit

---
+++ src/main/java/com/yeiimaccdev/springboot/apirest/ApiRestSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.yeiimaccdev.springboot.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSprintBootApplication.class, args);
+	}
+
+}
