24e6a7e1b4bf5478cf38062cd8bc65b5054bd04c
YeiiMacc <yeiimaccdev@gmail.com>
2022-03-02 12:38:29 -0500

    ClientRestController findall

---
+++ src/main/java/com/yeiimaccdev/springboot/apirest/controllers/ClientRestController.java
@@ -0,0 +1,24 @@
+package com.yeiimaccdev.springboot.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yeiimaccdev.springboot.apirest.models.services.IClientService;
+import com.yeiimaccdev.springboot.apirest.models.entity.Client;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientRestController {
+
+	@Autowired
+	private IClientService clientService;
+	
+	@GetMapping("") 
+	public List<Client> index() {
+		return clientService.findAll();
+	}
+}
