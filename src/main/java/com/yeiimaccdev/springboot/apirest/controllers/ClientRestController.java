81377403fedbd86ebf2302f8660ddba99b569dda
YeiiMacc <yeiimaccdev@gmail.com>
2022-03-02 23:24:15 -0500

    Cors and import.sql

--- src/main/java/com/yeiimaccdev/springboot/apirest/controllers/ClientRestController.java
+++ src/main/java/com/yeiimaccdev/springboot/apirest/controllers/ClientRestController.java
@@ -3,6 +3,7 @@ package com.yeiimaccdev.springboot.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.yeiimaccdev.springboot.apirest.models.entity.Client;
 
 @RestController
 @RequestMapping("/api/clients")
+@CrossOrigin(origins = {"http://localhost:4200/"})
 public class ClientRestController {
 
 	@Autowired
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clients (created_at, email, first_name, last_name, updated_at) VALUES ('2022-03-01', 'andrea@gmail.com', 'andrea', 'test2', '2022-03-02');
+INSERT INTO clients (created_at, email, first_name, last_name, updated_at) VALUES ('2022-02-23', 'luis@gmail.com', 'luis', 'castro', '2022-02-23');
+INSERT INTO clients (created_at, email, first_name, last_name, updated_at) VALUES ('2021-12-29', 'camila@gmail.com', 'camila', 'forero', '2021-12-29');
+INSERT INTO clients (created_at, email, first_name, last_name, updated_at) VALUES ('2021-11-19', 'angie@gmail.com', 'angie', 'zambrano', '2021-11-19');
+INSERT INTO clients (created_at, email, first_name, last_name, updated_at) VALUES ('2021-06-11', 'christian@gmail.com', 'christian', 'nodal', '2021-06-11');
