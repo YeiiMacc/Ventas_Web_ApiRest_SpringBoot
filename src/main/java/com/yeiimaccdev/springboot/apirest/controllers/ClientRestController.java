9ba79e502650df9b2e32be708d6687afc5d139e8
YeiiMacc <yeiimaccdev@gmail.com>
2022-03-12 14:56:39 -0500

    Error handling - show id.

--- src/main/java/com/yeiimaccdev/springboot/apirest/controllers/ClientRestController.java
+++ src/main/java/com/yeiimaccdev/springboot/apirest/controllers/ClientRestController.java
@@ -1,9 +1,13 @@
 package com.yeiimaccdev.springboot.apirest.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +36,24 @@ public class ClientRestController {
 	}
 	
 	@GetMapping("/{id}")
-	public Client show(@PathVariable Long id) {
-		return clientService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Client client = null;
+		Map<String, Object> response= new HashMap<>();
+		
+		try {
+			client = clientService.findById(id);
+		} catch (DataAccessException e) {
+			response.put("messagge", "Query error.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(client == null) {
+			response.put("messagge", "Error - Client with ID:".concat(id.toString().toString()).concat(" not found"));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Client>(client, HttpStatus.OK);
 	}
 	
 	@PostMapping("/save")
