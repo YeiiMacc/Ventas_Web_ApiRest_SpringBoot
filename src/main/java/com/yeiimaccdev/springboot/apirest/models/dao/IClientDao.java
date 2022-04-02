22b3785414ac1daf6f83822bbdfe8957b1ec14a7
YeiiMacc <yeiimaccdev@gmail.com>
2022-04-02 16:21:17 -0500

    Pagination - Spring data JPA

--- src/main/java/com/yeiimaccdev/springboot/apirest/models/dao/IClientDao.java
+++ src/main/java/com/yeiimaccdev/springboot/apirest/models/dao/IClientDao.java
@@ -1,9 +1,9 @@
 package com.yeiimaccdev.springboot.apirest.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yeiimaccdev.springboot.apirest.models.entity.Client;
 
-public interface IClientDao extends CrudRepository<Client, Long> {
+public interface IClientDao extends JpaRepository<Client, Long> {
 
 }
--- src/main/java/com/yeiimaccdev/springboot/apirest/models/services/ClientService.java
+++ src/main/java/com/yeiimaccdev/springboot/apirest/models/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,12 @@ public class ClientService implements IClientService {
 	@Transactional(readOnly = true)
 	public List<Client> findAll() {
 		return (List<Client>) clientDao.findAll();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Client> findAll(Pageable pageable) {
+		return clientDao.findAll(pageable);
 	}
 
 	@Override
